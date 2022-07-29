5d2337646d852d6d8179090ed62d85d9670d6de9
Morgan Schumann <91375960+morg-an@users.noreply.github.com>
2022-07-28 18:03:07 -0700

    Update app.py

--- Make_API/app.py
+++ Make_API/app.py
@@ -1,40 +1,51 @@
+from crypt import methods
+from lib2to3.pgen2.token import NEWLINE
+from urllib import request
 import numpy as np
 import pandas as pd
-from flask import Flask
+import csv
+from flask import Flask, render_template
+from flask import request
 from flask_cors import CORS, cross_origin
 import json
 app = Flask(__name__)
 cors = CORS(app)
 
-csv_list = pd.read_csv('todo.csv')
+# necessary for /update to work
+@ app.route('/')
+def index():
+    return render_template('new.html')
 
-# @ app.route('/')
-# def home():
-#     return csv_list.to_json(orient="index")
-
-# Returns contents of CSV as JSON
-
-
+# gets csv list and returns as JSON
 @ app.route('/get')
 def home():
+    csv_list = pd.read_csv('todo.csv')
     result = csv_list.to_json(orient="index")
-    # returns dict object
-    parsed = json.loads(result)
-    dumped = json.dumps(parsed, indent=4)
     return dumped
 
-# Returns contents of CSV as Python Dict.
-
-
+# gets csv list and converts it into a dictionary
 @app.route('/new')
 def new():
+    csv_list = pd.read_csv('todo.csv')
     result = csv_list.to_json(orient="index")
     parsed = json.loads(result)
     return parsed
 
+# Gets form input for new task as dictionary
+# Writes CSV file with a header line of column names
+# Iterates through all to-dos and writes each row to CSV
+@app.route('/update', methods=['GET', 'POST'])
+def update():
+    output = request.get_data()
+    result = json.loads(output)
+    csv_columns = ['todo', 'start_date', 'due_date']
+    with open('todo.csv', 'w', newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+        writer.writeheader()
+        for todo in result:
+            writer.writerow(result[todo])
+     return result
 
 # Notes to self:
-# Assign path to URL            @app.route("/")
 # Select virtual environment    View > Command Palette > Python: Select Interpreter
 # Run app                       "python3 -m flask run" in the terminal
-
